1d63ae3c4d38da2d2060982f0ae6554a06f4df83
fs65 <55656397+fs65@users.noreply.github.com>
2020-03-10 17:13:11 +0000

    game start

--- src/main.c
+++ src/main.c
@@ -1,1 +1,27 @@
+#include "cw2.c"
 
+int int main(int argc, char const *argv[]) {
+  /* code */
+
+  mmap_setup();
+  gpio_setup();
+
+  printf("\nBeginning Game\n");
+
+  int start = game();
+
+  if(start) {
+
+    printf("You Win!!\n");
+
+    blink(GREEN);
+
+
+  }
+
+  else {
+
+    printf("You Lost!\n");
+  }
+  return 0;
+}
