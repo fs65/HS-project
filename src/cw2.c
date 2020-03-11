2767c5e0fa6dcc13f9bc9a772d5f40c8bf6c4d14
fs65 <55656397+fs65@users.noreply.github.com>
2020-03-11 11:10:34 +0000

    removed some syntax errors

--- src/cw2.c
+++ src/cw2.c
@@ -34,13 +34,13 @@ void mmap_setup() {
   //sets up RPI memory access
   gpiobase = 0x3F200000;
 
-  if((fd = open ("/dev/mem", O_RDWR, O_SYNC, O_CLOEXEX)) < 0) {
+  if((fd = open ("/dev/mem", O_RDWR | O_SYNC | O_CLOEXEC)) < 0) {
 
     printf("Unable to open the required source")
     exit(1);
   }
 
-  gpio = (uint32_t *)mmap(0, BLOCK_SIZE, PROT_READ|PROT_WRITE, MAP_SHARED, fd, gpiobase);
+  gpio = (uint32_t *)mmap(0, BLOCK_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, gpiobase);
 
   if((uint32_t)gpio == -1) {
 
